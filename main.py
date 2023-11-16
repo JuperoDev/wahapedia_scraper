5bda869121f3bc60acae014e504b14e181c23fdc
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-11-16 14:06:51 +0100

    updated and integrated Ranged Weapons
    There is a bug. The module wasn't designed properly and some stuff due its format won't be saved properluy. So far it gets the name and modifiers perfectly. so the rest can be easily done by hand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
 import os
+from scraping_modules import parentUnit, factionKeywords, supremeCommander, damaged, lore, keywords
+from scraping_modules.leader import scrape_leader
+from scraping_modules.attributes import scrape_attributes
+from scraping_modules import rangedWeapons  # Import the rangedWeapons module
 
 def save_json(data, filename):
     folder_path = 'fetched-units'
@@ -9,9 +13,47 @@ def save_json(data, filename):
         json.dump(data, json_file, indent=2, ensure_ascii=False)
         print(f"JSON file created: {file_path}")
 
-from scraping_modules import parentUnit, factionKeywords, supremeCommander, damaged, lore, keywords
-from scraping_modules.leader import scrape_leader
-from scraping_modules.attributes import scrape_attributes  # Import the scrape_attributes function
+def main():
+    url_to_scrape = "https://wahapedia.ru/wh40k10ed/factions/adepta-sororitas/Aestred-Thurga-And-Agathae-Dolan"
+
+    scraped_data = parentUnit.scrape_parent_unit(url_to_scrape)
+
+    if scraped_data:
+        filename = f"{scraped_data['parentUnit'].replace(' ', '-').lower()}.json"
+
+        faction_keywords_data = factionKeywords.scrape_data(url_to_scrape)
+        scraped_data['factionKeywords'] = faction_keywords_data
+
+        damaged_data = damaged.scrape_damage_info(url_to_scrape)
+        scraped_data['damaged'] = damaged_data
+
+        supreme_commander_present = supremeCommander.check_supreme_commander(url_to_scrape)
+        scraped_data['supremeCommander'] = supreme_commander_present
+
+        lore_text = lore.get_pic_legend_title(url_to_scrape)
+        scraped_data['lore'] = lore_text
+
+        keywords_list = keywords.scrape_keywords(url_to_scrape)
+        scraped_data['keywords'] = keywords_list
+
+        # Use the URL from main.py to scrape attributes
+        attributes_data = scrape_attributes(url_to_scrape)
+        scraped_data['attributes'] = attributes_data
+
+        # Use the URL from main.py to scrape leader data
+        leader_list = scrape_leader(url_to_scrape)
+        scraped_data['leader'] = leader_list  # Include leader data in scraped_data
+
+        # Use the URL from main.py to scrape ranged weapons data
+        ranged_weapons_data = rangedWeapons.scrape_ranged_weapons(url_to_scrape)
+        scraped_data['rangedWeapons'] = ranged_weapons_data  # Include ranged weapons data in scraped_data
+
+        # Print the scraped rangedWeapons data
+        print("Scraped Ranged Weapons Data:")
+        print(json.dumps(ranged_weapons_data, indent=2, ensure_ascii=False))
+
+        save_json(scraped_data, filename)
+        print_fetched_data(scraped_data)
 
 def print_fetched_data(scraped_data):
     print("Fetched data:")
@@ -48,40 +90,6 @@ def print_fetched_data(scraped_data):
     # Print attributes information directly as an array
     print('"attributes":')
     print(json.dumps(scraped_data["attributes"], indent=2, ensure_ascii=False))
-
-def main():
-    url_to_scrape = "https://wahapedia.ru/wh40k10ed/factions/adepta-sororitas/Aestred-Thurga-And-Agathae-Dolan"
-
-    scraped_data = parentUnit.scrape_parent_unit(url_to_scrape)
-
-    if scraped_data:
-        filename = f"{scraped_data['parentUnit'].replace(' ', '-').lower()}.json"
-
-        faction_keywords_data = factionKeywords.scrape_data(url_to_scrape)
-        scraped_data['factionKeywords'] = faction_keywords_data
-
-        damaged_data = damaged.scrape_damage_info(url_to_scrape)
-        scraped_data['damaged'] = damaged_data
-
-        supreme_commander_present = supremeCommander.check_supreme_commander(url_to_scrape)
-        scraped_data['supremeCommander'] = supreme_commander_present
-
-        lore_text = lore.get_pic_legend_title(url_to_scrape)
-        scraped_data['lore'] = lore_text
-
-        keywords_list = keywords.scrape_keywords(url_to_scrape)
-        scraped_data['keywords'] = keywords_list
-
-        # Use the URL from main.py to scrape attributes
-        attributes_data = scrape_attributes(url_to_scrape)
-        scraped_data['attributes'] = attributes_data
-
-        # Use the URL from main.py to scrape leader data
-        leader_list = scrape_leader(url_to_scrape)
-        scraped_data['leader'] = leader_list  # Include leader data in scraped_data
-
-        save_json(scraped_data, filename)
-        print_fetched_data(scraped_data)
 
 if __name__ == "__main__":
     main()
--- scraping_modules/rangedWeapons.py
+++ scraping_modules/rangedWeapons.py
@@ -3,119 +3,109 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-# URL of the webpage
-url = "https://wahapedia.ru/wh40k10ed/factions/tyranids/Termagants"
 
-# Send a GET request to the URL
-response = requests.get(url)
+def scrape_ranged_weapons(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        ds_left_col_div = soup.find('div', class_='dsLeftСol')
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
+        if ds_left_col_div:
+            wtable = ds_left_col_div.find('table', class_='wTable')
 
-    # Find the div with class "dsLeftСol"
-    ds_left_col_div = soup.find('div', class_='dsLeftСol')
+            if wtable:
+                if 'RANGED' in wtable.text:
+                    rows = wtable.find_all('tr')
+                    print_enabled = True
+                    ranged_weapons = []
 
-    if ds_left_col_div:
-        # Find the first table with class "wTable" within the dsLeftСol div
-        wtable = ds_left_col_div.find('table', class_='wTable')
+                    for row in rows:
+                        text = row.get_text().strip()
+                        if "MELEE WEAPONSRANGEAWSSAPD" in text:
+                            print_enabled = False
+                        if "RANGED WEAPONSRANGEABSSAPD" in text:
+                            text = text.replace("RANGED WEAPONSRANGEABSSAPD", "")
+                        if print_enabled:
+                            if '[' in text:
+                                weapon_name, modifiers_and_rest = re.split(r'\[', text, 1)
+                                modifiers_and_rest = "[" + modifiers_and_rest
+                                modifiers_match = re.search(r'\[(.*?)\]', modifiers_and_rest)
 
-        if wtable:
-            # Check if the word "RANGED" is found in the table
-            if 'RANGED' in wtable.text:
-                # Find all <tr> elements within the table
-                rows = wtable.find_all('tr')
+                                if modifiers_match:
+                                    modifiers_str = modifiers_match.group(1)
+                                    modifiers = [modifier.strip() for modifier in modifiers_str.split(',')]
+                                    rest = modifiers_and_rest.split(modifiers_str)[-1].strip()
+                                    range_match = re.search(r'\](\d+)\"', rest)
 
-                # Initialize a flag to keep track of when to stop printing
-                print_enabled = True
+                                    if range_match:
+                                        range_value = int(range_match.group(1))
+                                    else:
+                                        range_value = None
 
-                # Initialize a list to store ranged weapons data
-                ranged_weapons = []
+                                    rest = rest.replace(f"]{range_value}\"", "").strip()
+                                    strength_match = re.search(r'[+A](\d+)(?=[-0])', rest)
 
-                # Iterate through each row and format the output
-                for row in rows:
-                    text = row.get_text().strip()
-                    if "MELEE WEAPONSRANGEAWSSAPD" in text:
-                        # Disable printing when "MELEE WEAPONSRANGEAWSSAPD" is found
-                        print_enabled = False
-                    if "RANGED WEAPONSRANGEABSSAPD" in text:
-                        # Exclude the specified string from printing
-                        text = text.replace("RANGED WEAPONSRANGEABSSAPD", "")
-                    if print_enabled:
-                        # Check if the line contains modifiers in square brackets
-                        if '[' in text:
-                            weapon_name, modifiers_and_rest = re.split(r'\[', text, 1)
-                            modifiers_and_rest = "[" + modifiers_and_rest
-                            modifiers_match = re.search(r'\[(.*?)\]', modifiers_and_rest)
-                            if modifiers_match:
-                                modifiers_str = modifiers_match.group(1)
-                                modifiers = [modifier.strip() for modifier in modifiers_str.split(',')]
-                                rest = modifiers_and_rest.split(modifiers_str)[-1].strip()
+                                    if strength_match:
+                                        strength = int(strength_match.group(1))
+                                        rest = rest.replace(f"+{strength}", "").strip()
+                                    else:
+                                        strength = None
 
-                                # Extract the range from "rest"
-                                range_match = re.search(r'\](\d+)\"', rest)
-                                if range_match:
-                                    range_value = int(range_match.group(1))
-                                else:
-                                    range_value = None
+                                    if rest and rest != "]":
+                                        weapon_name = weapon_name.replace("\u2013", "-")
+                                        ranged_weapon = {
+                                            "name": weapon_name.strip(),
+                                            "modifiers": modifiers,
+                                            "range": range_value,
+                                            "strength": strength,
+                                            "attacks": 0,
+                                            "ballistic-skills": 0,
+                                            "armor-penetration": 0,
+                                            "damage": 0,
+                                            "singleChoice": False
+                                        }
+                                        ranged_weapons.append(ranged_weapon)
+                            else:
+                                ranged_weapon = {
+                                    "name": text.strip(),
+                                    "modifiers": [],
+                                    "range": None,
+                                    "strength": None,
+                                    "attacks": 0,
+                                    "ballistic-skills": 0,
+                                    "armor-penetration": 0,
+                                    "damage": 0,
+                                    "singleChoice": False
+                                }
+                                ranged_weapons.append(ranged_weapon)
 
-                                # Remove "]18\"" or similar from "rest"
-                                rest = rest.replace(f"]{range_value}\"", "").strip()
-
-                                # Extract the "strength" attribute
-                                strength_match = re.search(r'[+A](\d+)(?=[-0])', rest)
-
-                                if strength_match:
-                                    strength = int(strength_match.group(1))
-                                    # Remove the extracted strength from "rest"
-                                    rest = rest.replace(f"+{strength}", "").strip()
-                                else:
-                                    strength = None
-
-                                if rest and rest != "]":
-                                    # Replace "\u2013" with "-"
-                                    weapon_name = weapon_name.replace("\u2013", "-")
-                                    ranged_weapon = {
-                                        "name": weapon_name.strip(),
-                                        "modifiers": modifiers,
-                                        "range": range_value,
-                                        "strength": strength,
-                                        "attacks": 0,  # Add attacks field with initial value
-                                        "ballistic-skills": 0,  # Add ballistic-skills field with initial value
-                                        "armor-penetration": 0,  # Add armor-penetration field with initial value
-                                        "damage": 0,  # Add damage field with initial value
-                                        "singleChoice": False  # Add singleChoice field with initial value
-                                    }
-                                    ranged_weapons.append(ranged_weapon)
-                        else:
-                            # If no modifiers are found, treat the entire line as the weapon name
-                            ranged_weapon = {
-                                "name": text.strip(),
-                                "modifiers": [],
-                                "range": None,
-                                "strength": None,
-                                "attacks": 0,  # Add attacks field with initial value
-                                "ballistic-skills": 0,  # Add ballistic-skills field with initial value
-                                "armor-penetration": 0,  # Add armor-penetration field with initial value
-                                "damage": 0,  # Add damage field with initial value
-                                "singleChoice": False  # Add singleChoice field with initial value
-                            }
-                            ranged_weapons.append(ranged_weapon)
-
-                # Filter out objects with both empty "rest" fields and "rest" equal to "]"
-                filtered_ranged_weapons = [weapon for weapon in ranged_weapons if weapon["range"] is not None]
-
-                # Create a new list without the "rest" field
-                filtered_ranged_weapons_without_rest = [{"name": weapon["name"], "modifiers": weapon["modifiers"], "range": weapon["range"], "strength": weapon["strength"], "attacks": weapon["attacks"], "ballistic-skills": weapon["ballistic-skills"], "armor-penetration": weapon["armor-penetration"], "damage": weapon["damage"], "singleChoice": weapon["singleChoice"]} for weapon in filtered_ranged_weapons]
-
-                # Print the formatted output
-                print('"rangedWeapons":', json.dumps(filtered_ranged_weapons_without_rest, indent=2))
+                    filtered_ranged_weapons = [weapon for weapon in ranged_weapons if weapon["range"] is not None]
+                    filtered_ranged_weapons_without_rest = [
+                        {
+                            "name": weapon["name"],
+                            "modifiers": weapon["modifiers"],
+                            "range": weapon["range"],
+                            "strength": weapon["strength"],
+                            "attacks": weapon["attacks"],
+                            "ballistic-skills": weapon["ballistic-skills"],
+                            "armor-penetration": weapon["armor-penetration"],
+                            "damage": weapon["damage"],
+                            "singleChoice": weapon["singleChoice"]
+                        }
+                        for weapon in filtered_ranged_weapons
+                    ]
+                    return filtered_ranged_weapons_without_rest
+                else:
+                    return []
             else:
-                print('"rangedWeapons": []')
+                return []
         else:
-            print("Table with class 'wTable' not found in dsLeftСol div.")
+            return []
     else:
-        print("Div with class 'dsLeftСol' not found on the page.")
-else:
-    print(f"Failed to retrieve the webpage (status code {response.status_code}).")
+        return []
+
+
+if __name__ == "__main__":
+    url_to_scrape = "https://wahapedia.ru/wh40k10ed/factions/adepta-sororitas/Aestred-Thurga-And-Agathae-Dolan"
+    ranged_weapons_data = scrape_ranged_weapons(url_to_scrape)
+    print(json.dumps(ranged_weapons_data, indent=2))
